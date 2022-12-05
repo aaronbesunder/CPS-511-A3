9765d7e0dc45a0e859cce3234407917d88a04e03
TaeDaeTao <72405276+TaeDaeTao@users.noreply.github.com>
2022-12-04 22:27:54 -0500

    Help updated with X and Y cannon angles

--- Robot3D.cpp
+++ Robot3D.cpp
@@ -1996,12 +1996,12 @@ void drawRightText()
 		}
 		else
 		{
-			sprintf_s(str, "Robot Angle: %0.1f\n\nBotThree: One Angles\n   Body: %0.1f\n   Hip: %0.1f\n   Knee: %0.1f\n\nBotThree: Two Angles\n   Body: %0.1f\n   Hip: %0.1f\n   Knee: %0.1f\n\nBotFour: One Angles\n   Body: %0.1f\n   Hip: %0.1f\n   Knee: %0.1f\n\nBotFour: Two Angles\n   Body: %0.1f\n   Hip: %0.1f\n   Knee: %0.1f\n\nRotation Speed: %0.1f\n\nCannon Angle: %0.1f", 
+			sprintf_s(str, "Robot Angle: %0.1f\n\nBotThree: One Angles\n   Body: %0.1f\n   Hip: %0.1f\n   Knee: %0.1f\n\nBotThree: Two Angles\n   Body: %0.1f\n   Hip: %0.1f\n   Knee: %0.1f\n\nBotFour: One Angles\n   Body: %0.1f\n   Hip: %0.1f\n   Knee: %0.1f\n\nBotFour: Two Angles\n   Body: %0.1f\n   Hip: %0.1f\n   Knee: %0.1f\n\nRotation Speed: %0.1f\n\nCannon Angle\n   X: %0.1f\n   Y: %0.1f", 
 				botThree_robotAngle, botThree_bodyAngle, botThreeOne_rightHipAngle, botThreeOne_rightKneeAngle,
 				botThree_bodyAngle, botThreeTwo_rightHipAngle, botThreeTwo_rightKneeAngle,
 				botFour_bodyAngle, botFour_leftHipAngle, botFour_leftKneeAngle, 
 				botFour_bodyAngle, botFour_leftHipAngle, botFour_leftKneeAngle,
-				rotateSpeed, cannon_rotateX);
+				rotateSpeed, cannon_rotateX, cannon_rotateY);
 		}
 		glutBitmapString(GLUT_BITMAP_HELVETICA_12, reinterpret_cast<const unsigned char*>
 			(str));
